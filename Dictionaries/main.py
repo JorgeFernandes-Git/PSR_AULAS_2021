edbf5546752deaaa194e16356772ea3661a07c04
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-10-30 14:07:40 +0100

    dict test

--- Dictionaries/main.py
+++ Dictionaries/main.py
@@ -1,25 +1,43 @@
 #!/usr/bin/python3
 
 from collections import namedtuple
+import pprint
+from time import time, ctime
 
 Input = namedtuple("Input", ["requested", "received", "duration"])
+my_dict = {
+    "accuracy": 0.0,
+    "inputs": [],
+    "number_of_hits": 0,
+    "number_of_types": 0,
+    "test_duration": 0.0,
+    "test_end": "",
+    "test_start": "",
+    "type_average_duration": 0.0,
+    "type_hit_average_duration": 0.0,
+    "type_miss_average_duration": 0.0
+}
 
 
 def main():
-    mydict = {
-        "accuracy": 0.0,
-        "inputs": [Input(requested="", received="", duration=0.0)],
-        "number_of_hits": 0,
-        "number_of_types": 5,
-        "test_duration": 0.0,
-        "test_end": "",
-        "test_start": "",
-        "type_average_duration": 0.0,
-        "type_hit_average_duration": 0.0,
-        "type_miss_average_duration": 0.0
-    }
+    # input tuples
+    a = Input(requested="w", received="d", duration=2.3)
+    b = Input(requested="r", received="f", duration=1.5)
+    c = Input(requested="i", received="y", duration=1.5)
 
-    print(mydict)
+    my_dict["accuracy"] = 2.8
+    my_dict["inputs"] = [a, b, c]
+    my_dict["number_of_hits"] = 5
+    my_dict["number_of_types"] = 8
+    my_dict["test_duration"] = 5.8
+    my_dict["test_end"] = ctime()
+    my_dict["test_start"] = ctime()
+    my_dict["type_average_duration"] = 2.6
+    my_dict["type_hit_average_duration"] = 1.5
+    my_dict["type_miss_average_duration"] = 5.4
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(my_dict)
 
 
 if __name__ == '__main__':
--- Proj1_teste/main.py
+++ Proj1_teste/main.py
@@ -6,10 +6,29 @@ import readchar
 from colorama import Fore, Back, Style
 from time import time, ctime
 import argparse
+from collections import namedtuple
+import pprint
+
+Input = namedtuple("Input", ["requested", "received", "duration"])
+my_dict = {
+    "accuracy": 0.0,
+    "inputs": [Input(requested="", received="", duration=0.0)],
+    "number_of_hits": 0,
+    "number_of_types": 0,
+    "test_duration": 0.0,
+    "test_end": "",
+    "test_start": "",
+    "type_average_duration": 0.0,
+    "type_hit_average_duration": 0.0,
+    "type_miss_average_duration": 0.0
+}
 
 
 def average(lst):
-    return sum(lst) / len(lst)
+    if len(lst) != 0:
+        return sum(lst) / len(lst)
+    else:
+        return 0
 
 
 def typing_test_max_val(max_value):
@@ -18,15 +37,21 @@ def typing_test_max_val(max_value):
     cnt_letters = 0
     random_letter = ""
     num_letters_to_type = max_value
+
     # agrupar e contar letras lidas
     correct_letters = []
     wrong_letters = []
     cnt_correct_letters = 0
     cnt_wrong_letters = 0
+
     # contagem de tempo
     init_time = time()
-    final_time = 0
+    interm_tot_time = time()
+    cnt_time = 0
+    interm_time = 0
     read_times = []
+    read_correct_times = []
+    read_incorrect_times = []
     avg_times = 0
 
     while True:
@@ -38,29 +63,47 @@ def typing_test_max_val(max_value):
             print("Type the letter: " + random_letter)
             typed_letter = readchar.readkey()
             if typed_letter == random_letter:
-                final_time = time() - init_time
-                read_times.append(final_time)
-                init_time = time()
+                interm_time = time() - interm_tot_time
+                read_times.append(interm_time)
+                read_correct_times.append(interm_time)
+                interm_tot_time = time()
                 cnt_correct_letters += 1
                 correct_letters.append(random_letter)
             else:
-                final_time = time() - init_time
-                read_times.append(final_time)
-                init_time = time()
+                interm_time = time() - interm_tot_time
+                read_times.append(interm_time)
+                read_incorrect_times.append(interm_time)
+                interm_tot_time = time()
                 cnt_wrong_letters += 1
                 wrong_letters.append(random_letter)
 
     # contabilizar tempos
     avg_times = average(read_times)
+    cnt_time = time() - init_time
+    avg_correct_times = average(read_correct_times)
+    avg_incorrect_times = average(read_incorrect_times)
 
-    print("Current time: " + Fore.RED + Style.BRIGHT + ctime() + Style.RESET_ALL)
-    print(Fore.RED + Style.BRIGHT + Back.YELLOW + "accuracy: " + str(avg_times) + " seconds" + Style.RESET_ALL)
-    print("You typed " + Fore.RED + str(cnt_correct_letters) + Style.RESET_ALL + " correct letters")
-    print(", ".join(correct_letters))
-    print("You typed " + Fore.RED + str(cnt_wrong_letters) + Style.RESET_ALL + " incorrect letters")
-    print(", ".join(wrong_letters))
-    print("Time per typed letter:")
-    print(read_times)
+    my_dict["accuracy"] = (cnt_correct_letters / (cnt_correct_letters + cnt_wrong_letters)) * 100
+    my_dict["number_of_hits"] = cnt_correct_letters
+    my_dict["number_of_types"] = cnt_correct_letters + cnt_wrong_letters
+    my_dict["test_duration"] = cnt_time
+    my_dict["test_end"] = ctime()
+    my_dict["test_start"] = ctime()
+    my_dict["type_average_duration"] = avg_times
+    my_dict["type_hit_average_duration"] = avg_correct_times
+    my_dict["type_miss_average_duration"] = avg_incorrect_times
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(my_dict)
+
+    # print("Current time: " + Fore.RED + Style.BRIGHT + ctime() + Style.RESET_ALL)
+    # print(Fore.RED + Style.BRIGHT + Back.YELLOW + "accuracy: " + str(avg_times) + " seconds" + Style.RESET_ALL)
+    # print("You typed " + Fore.RED + str(cnt_correct_letters) + Style.RESET_ALL + " correct letters")
+    # print(", ".join(correct_letters))
+    # print("You typed " + Fore.RED + str(cnt_wrong_letters) + Style.RESET_ALL + " incorrect letters")
+    # print(", ".join(wrong_letters))
+    # print("Time per typed letter:")
+    # print(read_times)
 
 
 def typing_test_max_time(max_value):
@@ -80,6 +123,8 @@ def typing_test_max_time(max_value):
     cnt_time = 0
     interm_time = 0
     read_times = []
+    read_correct_times = []
+    read_incorrect_times = []
     avg_times = 0
 
     while True:
@@ -93,27 +138,44 @@ def typing_test_max_time(max_value):
             if typed_letter == random_letter:
                 interm_time = time() - interm_tot_time
                 read_times.append(interm_time)
+                read_correct_times.append(interm_time)
                 interm_tot_time = time()
                 cnt_correct_letters += 1
                 correct_letters.append(random_letter)
             else:
                 interm_time = time() - interm_tot_time
                 read_times.append(interm_time)
+                read_incorrect_times.append(interm_time)
                 interm_tot_time = time()
                 cnt_wrong_letters += 1
                 wrong_letters.append(random_letter)
 
     # contabilizar tempos
     avg_times = average(read_times)
+    avg_correct_times = average(read_correct_times)
+    avg_incorrect_times = average(read_incorrect_times)
 
-    print("Current time: " + Fore.RED + Style.BRIGHT + ctime() + Style.RESET_ALL)
-    print(Fore.RED + Style.BRIGHT + Back.YELLOW + "accuracy: " + str(avg_times) + " seconds" + Style.RESET_ALL)
-    print("You typed " + Fore.RED + str(cnt_correct_letters) + Style.RESET_ALL + " correct letters")
-    print(", ".join(correct_letters))
-    print("You typed " + Fore.RED + str(cnt_wrong_letters) + Style.RESET_ALL + " incorrect letters")
-    print(", ".join(wrong_letters))
-    print("Time per typed letter:")
-    print(read_times)
+    my_dict["accuracy"] = (cnt_correct_letters / (cnt_correct_letters + cnt_wrong_letters)) * 100
+    my_dict["number_of_hits"] = cnt_correct_letters
+    my_dict["number_of_types"] = cnt_correct_letters + cnt_wrong_letters
+    my_dict["test_duration"] = cnt_time
+    my_dict["test_end"] = ctime()
+    my_dict["test_start"] = ctime()
+    my_dict["type_average_duration"] = avg_times
+    my_dict["type_hit_average_duration"] = avg_correct_times
+    my_dict["type_miss_average_duration"] = avg_incorrect_times
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(my_dict)
+
+    # print("Current time: " + Fore.RED + Style.BRIGHT + ctime() + Style.RESET_ALL)
+    # print(Fore.RED + Style.BRIGHT + Back.YELLOW + "accuracy: " + str(avg_times) + " seconds" + Style.RESET_ALL)
+    # print("You typed " + Fore.RED + str(cnt_correct_letters) + Style.RESET_ALL + " correct letters")
+    # print(", ".join(correct_letters))
+    # print("You typed " + Fore.RED + str(cnt_wrong_letters) + Style.RESET_ALL + " incorrect letters")
+    # print(", ".join(wrong_letters))
+    # print("Time per typed letter:")
+    # print(read_times)
 
 
 def main():
