512f186b4096ddbfc91681ed8dac334b10eb4390
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-11-04 12:38:53 +0000

    open cv exercises

---
+++ openCV/Ex1/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import argparse
+import time
+
+import cv2
+import readchar
+
+
+def main():
+    # parser = argparse.ArgumentParser(description="Open image in path")
+    # parser.add_argument("-img", "--image", type=str, help="PATH TO IMAGE")
+    # args = vars(parser.parse_args())
+    # print(args)
+
+    image_1 = "/home/jorge/Desktop/UA/PSR/Pycharm_EX/psr_21-22/Parte05/images/atlascar.png"
+    image_2 = "/home/jorge/Desktop/UA/PSR/Pycharm_EX/psr_21-22/Parte05/images/atlascar2.png"
+    image1 = cv2.imread(image_1, cv2.IMREAD_COLOR)  # Load an image
+    image2 = cv2.imread(image_2, cv2.IMREAD_COLOR)
+
+    # image1 = cv2.imread(args["image"], cv2.IMREAD_COLOR)  # Load an image
+
+    while True:
+        cv2.imshow('window1', image1)  # Display the image
+        cv2.waitKey(1000)
+        cv2.imshow('window1', image2)  # Display the image
+        cv2.waitKey(1000)  # wait for a key press before proceeding in milliseconds
+        # pressed_continue = readchar.readkey()
+        # if pressed_continue:
+        #     exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ openCV/Ex2/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import argparse
+import time
+import numpy as np
+
+import cv2
+import readchar
+
+
+def main():
+    # path to image
+    image_1 = "/home/jorge/Desktop/UA/PSR/Pycharm_EX/psr_21-22/Parte05/images/atlascar.png"
+    image1 = cv2.imread(image_1, cv2.IMREAD_COLOR)  # Load an image
+    image_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)  # convert to gray scale
+
+    # process the image
+    _, image_negative = cv2.threshold(image_gray, 128, 255, cv2.THRESH_BINARY)
+
+    # show the images in 3 windows
+    cv2.imshow("original", image1)
+    cv2.imshow("gray", image_gray)
+    cv2.imshow("img_threshold", image_negative)
+
+    cv2.waitKey(8000)  # wait for 8 seconds
+
+
+if __name__ == '__main__':
+    main()
