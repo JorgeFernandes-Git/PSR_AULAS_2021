8472f528b03e03306982a8e825ae522e152cb3c7
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-11-06 00:36:11 +0000

    open cv exercises

--- openCV/Ex2/main2.py
+++ openCV/Ex2/main2.py
@@ -13,20 +13,24 @@ def main():
     image1 = cv2.imread(image_1, cv2.IMREAD_COLOR)  # Load an image
     image_b, image_g, image_r = cv2.split(image1)  # split the image
 
-    ranges = {"b": {"min": 0, "max": 100},
+    ranges = {"b": {"min": 0, "max": 60},
               "g": {"min": 80, "max": 256},
-              "r": {"min": 0, "max": 100},
+              "r": {"min": 0, "max": 60},
               }
 
     mins = np.array([ranges['b']['min'], ranges['g']['min'], ranges['r']['min']])
     maxs = np.array([ranges['b']['max'], ranges['g']['max'], ranges['r']['max']])
     image_process = cv2.inRange(image1, mins, maxs)
+    image_hsv = cv2.cvtColor(image1, cv2.COLOR_BGR2HSV)
 
     # show the images
-    # cv2.namedWindow("image1", cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow("image1", cv2.WINDOW_NORMAL)
+    cv2.imshow("image1", image1)
     cv2.imshow("image_process", image_process)
+    cv2.imshow("image_hsv", image_hsv)
 
     cv2.waitKey(0)  # wait for 8 seconds
+    cv2.destroyAllWindows() # Esc to close
 
 
 if __name__ == '__main__':
---
+++ openCV/Ex3/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import argparse
+import cv2
+
+# Global variables
+window_name = 'window - Ex3a'
+image_gray = None
+
+
+def onTrackbar(threshold):
+    # Add code here to threshold image_gray and show image in window
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--image', type=str, required=True,help='Full path to image file.')
+    args = vars(parser.parse_args())
+
+    image = cv2.imread(args['image'], cv2.IMREAD_COLOR)  # Load an image
+    global image_gray # use global var
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert bgr to gray image (single channel)
+    cv2.namedWindow(window_name)
+
+    # add code to create the trackbar ...
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
