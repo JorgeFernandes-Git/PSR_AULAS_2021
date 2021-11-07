03e423502493bceda33c86ab054ff8ad68560e83
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-11-07 17:54:24 +0000

    face detection update

--- openCV/video_capture/face_detection/main.py
+++ openCV/video_capture/face_detection/main.py
@@ -4,14 +4,13 @@ import cv2 as cv
 
 
 def onTrackbar(threshold):
-    print("Selected threshold " + str(threshold) + " for limit")
+    # print("Selected threshold " + str(threshold) + " for limit")
+    pass
 
 
 def main():
-    # flags for select the modes
-    gray_bool = False
-    mask_bool = False
-
+    # load the cascade
+    face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
     # initial setup
     capture = cv.VideoCapture(0)  # connect to webcam
 
@@ -19,48 +18,27 @@ def main():
     window_name = 'Video_Capture'
     cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
 
-    # create trackbars
-    cv.createTrackbar("min", window_name, 0, 255, onTrackbar)
-    cv.createTrackbar("max", window_name, 0, 255, onTrackbar)
-
     # cycle for continuously capture the image
     while True:
-
         # read the image
         _, image = capture.read()
+        # Convert to grayscale
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        if not gray_bool:
-            # show the image
-            cv.imshow(window_name, image)
-
-        if gray_bool:
-            # convert to gray scale
-            gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-            cv.imshow(window_name, gray)
-
-        if mask_bool:
-            min_gray = cv.getTrackbarPos("min", window_name)
-            max_gray = cv.getTrackbarPos("max", window_name)
-            gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-            mask = cv.inRange(gray, min_gray, max_gray)
-            cv.imshow(window_name, mask)
+        # Display
+        cv.imshow(window_name, image)
 
         # ESC to close
-        k = cv.waitKey(1) & 0xFF
+        k = cv.waitKey(30) & 0xFF
         if k == 27:
             break
 
-        if k == ord("g"):
-            gray_bool = True
-            mask_bool = False
-        if k == ord("r"):
-            gray_bool = False
-            mask_bool = False
-        if k == ord("m"):
-            mask_bool = True
-            gray_bool = False
-
-    capture.release()  # free the webcam for other use
+    capture.release()  # free the webcam for other uses if needed
     cv.destroyAllWindows()
 
 
