925c5df02387b96e1a36f7c46747d9166d315d2d
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-11-18 20:52:06 +0000

    talker and listener

--- ROS/First_Exercises/Listener/main.py
+++ ROS/First_Exercises/Listener/main.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 import rospy
 from std_msgs.msg import String
 
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + " Look another car at " + str(data.data))
 
 
 def main():
@@ -13,9 +13,10 @@ def main():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.init_node('Porto', anonymous=True)
+
+    rospy.Subscriber("A1", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- ROS/First_Exercises/Talker/main.py
+++ ROS/First_Exercises/Talker/main.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
 
 
 def main():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    pub = rospy.Publisher('A1', String, queue_size=10)
+    rospy.init_node('Aveiro', anonymous=True)
+    rate = rospy.Rate(1)  # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = "Here goes a car at " + str(rospy.get_time())
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
