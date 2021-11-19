b46de0086dc9ab37ebda10fbbd0064d741b1983c
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-11-19 18:47:35 +0000

    face recognition

--- openCV/face_recognition/attendance_project.py
+++ openCV/face_recognition/attendance_project.py
@@ -4,9 +4,16 @@ import cv2
 import numpy as np
 import face_recognition
 
-def main():
 
+def main():
+    # load image
+    img_original = face_recognition.load_image_file("ElonMusk1.jpeg")
+    img_original = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
+
+    # image to test landmarks
+    img_test = face_recognition.load_image_file("bill_gates.jpg")
+    img_test = cv2.cvtColor(img_test, cv2.COLOR_BGR2RGB)
 
 
 if __name__ == '__main__':
-   main()
+    main()
