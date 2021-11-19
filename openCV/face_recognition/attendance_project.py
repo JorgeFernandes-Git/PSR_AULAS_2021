9e49713a9f78cb4b8ca1c8dd12c2a754a777e5a8
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-11-19 18:26:26 +0000

    face recognition

---
+++ openCV/face_recognition/attendance_project.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import cv2
+import numpy as np
+import face_recognition
+
+def main():
+
+
+
+if __name__ == '__main__':
+   main()
--- openCV/face_recognition/main.py
+++ openCV/face_recognition/main.py
@@ -7,34 +7,36 @@ import face_recognition
 
 def main():
     # load image
-    img_elon = face_recognition.load_image_file("ElonMusk1.jpeg")
-    img_elon = cv2.cvtColor(img_elon, cv2.COLOR_BGR2RGB)
+    img_original = face_recognition.load_image_file("ElonMusk1.jpeg")
+    img_original = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
 
     # image to test landmarks
-    img_test = face_recognition.load_image_file("ElonMusk2.jpg")
+    img_test = face_recognition.load_image_file("bill_gates.jpg")
     img_test = cv2.cvtColor(img_test, cv2.COLOR_BGR2RGB)
 
     # detect the face
-    face_loc = face_recognition.face_locations(img_elon)[0]
-    encode_elon = face_recognition.face_encodings(img_elon)[0]
+    face_loc = face_recognition.face_locations(img_original)[0]
+    encode_original = face_recognition.face_encodings(img_original)[0]
     # make the rectangle
-    cv2.rectangle(img_elon, (face_loc[3], face_loc[0]), (face_loc[1], face_loc[2]), (255, 0, 255), 2)
+    cv2.rectangle(img_original, (face_loc[3], face_loc[0]), (face_loc[1], face_loc[2]), (255, 0, 255), 2)
 
     # detect the face test
     face_loc_test = face_recognition.face_locations(img_test)[0]
     encode_test = face_recognition.face_encodings(img_test)[0]
     # make the rectangle
-    cv2.rectangle(img_test, (face_loc_test[3], face_loc_test[0]), (face_loc_test[1], face_loc_test[2]), (255, 0, 255), 2)
+    cv2.rectangle(img_test, (face_loc_test[3], face_loc_test[0]),
+                  (face_loc_test[1], face_loc_test[2]), (255, 0, 255), 2)
 
-    # compare the encodings
-    results = face_recognition.compare_faces([encode_elon], encode_test)
-    face_dist = face_recognition.face_distance([encode_elon], encode_test)
-    print(results)
-    print(face_dist)
+    # compare the encodings (original vs test)
+    results = face_recognition.compare_faces([encode_original], encode_test)    # true or false
+    face_dist = face_recognition.face_distance([encode_original], encode_test)  # number bitween 0 and 1
+    print(results, face_dist)
+    cv2.putText(img_test, f'{results} {round(face_dist[0], 2)}',
+                (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)  # display the results o the test image
 
     # show the images
-    cv2.imshow("Elon Musk", img_elon)
-    cv2.imshow("Elon Test", img_test)
+    cv2.imshow("Original", img_original)
+    cv2.imshow("Test", img_test)
     cv2.waitKey(0)
 
 
