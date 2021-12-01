6894808329496e640bb1d099cfb4afc871f9a95c
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-12-01 05:07:10 +0000

    hand detection

--- openCV/Hand_tracking/hand_tracking_class.py
+++ openCV/Hand_tracking/hand_tracking_class.py
@@ -5,7 +5,7 @@ import mediapipe as mp
 import time
 
 
-class HandDetector:
+class HandDetector():
 
     def __init__(self, mode=False, max_hands=2, detection_confidant=0.5, track_confidant=0.5):
         self.mode = mode
@@ -24,32 +24,33 @@ class HandDetector:
         # print(results)
 
         if self.results.multi_hand_landmarks:
-            for hand_lms in  :
+            for hand_lms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mp_draw.draw_landmarks(img, hand_lms,
                                                 self.mp_hands.HAND_CONNECTIONS)  # hand_lms is single hand
 
         return img
 
-        def find_position(self, img, hand_num=0, draw=True):
-            lm_list = []
-            if self.results.multi_hand_landmarks:
-                my_hand = self.results.multi_hand_landmarks[hand_num]
+    def find_position(self, img, hand_num=0, draw=True):
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_num]
 
-                for id, lm in enumerate(my_hand.landmark):
-                    # print(id, lm)
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    # print(id, cx, cy)
-                    lmz
-                    if id == 0:  # id of the landmark (21 in total)
-                        cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-            return
+            for id, lm in enumerate(my_hand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lm_list.append([id, cx, cy])
+                # if id == 0:  # id of the landmark (21 in total)
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        return lm_list
 
 
 def main():
     cap = cv2.VideoCapture(0)
-    detector = HandDetector
+    detector = HandDetector()
 
     prev_time = 0
     cur_time = 0
@@ -57,6 +58,10 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
+
+        if len(lm_list) != 0:
+            print(lm_list[4])
 
         # number of frames
         cur_time = time.time()
