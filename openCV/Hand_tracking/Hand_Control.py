ba5997366d2947a4dc2ef028edf461051df5ee4b
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-12-01 18:50:35 +0000

    hand control

--- openCV/Hand_tracking/Hand_Control.py
+++ openCV/Hand_tracking/Hand_Control.py
@@ -4,9 +4,6 @@ import cv2
 import time
 import numpy
 
-# camera param
-w_cam, h_cam = 640, 480
-
 
 def main():
     # camera param
@@ -25,7 +22,7 @@ def main():
         fps = 1 / (cur_time - prev_time)
         prev_time = cur_time
 
-        # put text on frame
+        # put text on frame (frame/seconds)
         cv2.putText(img, f'FPS: {str(int(fps))}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
 
         # show the image
