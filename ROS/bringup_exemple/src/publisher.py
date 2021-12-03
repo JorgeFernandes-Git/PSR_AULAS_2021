867f4dd2304b083b59e5b94a636373a2db9a343a
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-12-03 01:01:39 +0000

    bring up exemple

--- ROS/bringup_exemple/src/publisher.py
+++ ROS/bringup_exemple/src/publisher.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-import argparse
+
 import rospy
-from std_msgs.msg import String
 from bringup_exemple.msg import Dog
 
 
-
 def main():
-    parser = argparse.ArgumentParser(description='Select the message to send, the topic and the rate')
-    parser.add_argument("-nd", "--node", type=str, default="Anywhere", help="Name the node to publish")
-    parser.add_argument('-msg', '--message', type=str, help='Message to send')
-    parser.add_argument('-tp', '--topic', type=str, default="Anywhere", help='Topic to send message')
-    parser.add_argument('-rt', '--rate', type=float, default=0.5, help='Message rate')
-    args = vars(parser.parse_args())
-
-    rospy.init_node(args["node"], anonymous=True)
-    pub = rospy.Publisher(args["topic"], Dog, queue_size=10)
-    rate = rospy.Rate(args["rate"])  # 10hz
+    rospy.init_node("publisher", anonymous=True)
+    pub = rospy.Publisher("chatter", Dog, queue_size=10)
+    rate = rospy.Rate(10)  # 10hz
 
     while not rospy.is_shutdown():
+        # create a dog message to send
         dog = Dog()
         dog.name = "bobby"
         dog.age = 77
@@ -27,10 +19,13 @@ def main():
         dog.brothers.append("Max")
         dog.brothers.append("Snoopy")
 
-        # message_to_send = args["message"]
-        rospy.loginfo("Sending a dog ...")
+        rospy.loginfo(f'Publishing dog message with name {dog.name}')
         pub.publish(dog)
         rate.sleep()
+
+        # commands for terminal
+        # rosrun bringup_exemple publisher.py __name:=my_publisher
+        # rosrun bringup_exemple publisher.py __name:=my_publisher chatter:=my_topic
 
 
 if __name__ == '__main__':
