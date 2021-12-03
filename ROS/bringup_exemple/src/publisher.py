2dd79d819982b6d033018554a2e026c030b7842e
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-12-03 01:44:16 +0000

    bring up exemple

--- ROS/bringup_exemple/src/publisher.py
+++ ROS/bringup_exemple/src/publisher.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
+
 import rospy
 from bringup_exemple.msg import Dog
+from colorama import Fore, Back, Style
 
 
 def main():
     rospy.init_node("publisher", anonymous=True)
-    pub = rospy.Publisher("chatter", Dog, queue_size=10)
-    rate = rospy.Rate(10)  # 10hz
+    pub = rospy.Publisher("chatter", Dog, queue_size=1)
+    # frequency
+    frequency = rospy.get_param("~frequency", default=1)  # "~" means private param
+    rate = rospy.Rate(frequency)  # 10hz
 
     while not rospy.is_shutdown():
+        # read param
+        highlight_text_color = rospy.get_param("/highlight_text_color")
+
         # create a dog message to send
         dog = Dog()
         dog.name = "bobby"
@@ -19,13 +26,22 @@ def main():
         dog.brothers.append("Max")
         dog.brothers.append("Snoopy")
 
-        rospy.loginfo(f'Publishing dog message with name {dog.name}')
+        rospy.loginfo(f'Publishing dog message with name '
+                      f'{getattr(Fore, highlight_text_color) + dog.name + Style.RESET_ALL}')
         pub.publish(dog)
         rate.sleep()
 
         # commands for terminal
         # rosrun bringup_exemple publisher.py __name:=my_publisher
         # rosrun bringup_exemple publisher.py __name:=my_publisher chatter:=my_topic
+
+        # edit param in ROS
+        # rosparam set highlight_text_color GREEN
+        # rosparam set /my_publisher/frequency
+
+        # rosparam list and rosparam get
+
+        # rosparam load params.yml -- run in director roscd package_name/params folder
 
 
 if __name__ == '__main__':
--- ROS/bringup_exemple/src/subscriber.py
+++ ROS/bringup_exemple/src/subscriber.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-import argparse
+
 import rospy
-from std_msgs.msg import String
 from bringup_exemple.msg import Dog
 
 
 def callback(msg):
-    rospy.loginfo(rospy.get_caller_id() + " Message Received: " + str(msg.name))
-    rospy.loginfo(rospy.get_caller_id() + " Message Received: " + str(msg.age))
-    rospy.loginfo(rospy.get_caller_id() + " Message Received: " + str(msg.color))
-    rospy.loginfo(rospy.get_caller_id() + " Message Received: " + str(msg.brothers))
+    rospy.loginfo(f'Received a dog name {msg.name} which is {msg.age} years old')
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Select the node and the topic to subscribe')
-    parser.add_argument('-nd', '--node', type=str, default="Lost", help='Name a node to subscribe')
-    parser.add_argument('-tp1', '--topic1', type=str, help='Name a topic1')
-    parser.add_argument('-tp2', '--topic2', type=str, help='Name a topic2')
-    args = vars(parser.parse_args())
-
-    rospy.init_node(args["node"], anonymous=True)
-    rospy.Subscriber(args["topic1"], Dog, callback)
-
-    if not args["topic2"] is None:
-        rospy.Subscriber(args["topic2"], Dog, callback)
+    rospy.init_node("listener", anonymous=True)
+    rospy.Subscriber("chatter", Dog, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
